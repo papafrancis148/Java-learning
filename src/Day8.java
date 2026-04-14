77a5b688c26ec5b5eba698af9e0e95bad2de6ca6
OGWUELEKA IKENNA <ikeogwueleka@gmaill.com>
2026-04-14 12:31:02 +0200

    "Day8- It's been a while but I am back today I learnt how to use Ternary Operators also learnt how to convert weight from lbs to kgs and kgs to lbs

---
+++ src/Day8.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Day8 {
+    public static void main(String[] args){
+        // WEIGHT CONVERSION PROGRAM
+/*        Scanner scanner = new Scanner(System.in);
+
+        double weight;
+        double newWeight;
+        int choice;
+
+        System.out.println("Weight Conversion Program");
+        System.out.println("1: Convert kgs to lbs.");
+        System.out.println("2: Convert lbs to kgs.");
+
+        System.out.print("Choose an option: ");
+        choice = scanner.nextInt();
+
+        if (choice == 1){
+            System.out.print("Enter the weight in lbs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 0.4;
+            System.out.printf("The weight in kgs is: %.2f kilograms.", newWeight);
+        } else if (choice == 2) {
+            System.out.print("Enter the weight in kgs: ");
+            weight = scanner.nextDouble();
+            newWeight = weight * 2.2;
+            System.out.printf("The weight in lbs is: %.2f pounds.", newWeight);
+        }
+        else {
+            System.out.println("The number you selected is not part of the option");
+        }
+*/
+
+        // TERNARY OPERATOR ? = In Java this operator returns 1 of 2 values if a condition is true. It is also an alternative to the IF/ELSE statements
+        // variable = (condition) ? ifTrue : ifFalse;
+/*
+        int score = 70;
+
+        String passOrFail = (score >= 65) ? "PASS" : "FAIL";
+        System.out.println(passOrFail);
+
+        int number = 3;
+
+        String evenOrOdd = (number % 2 == 0) ? "EVEN" : "ODD";
+        System.out.println(evenOrOdd);
+
+
+
+        int hours = 9;
+
+        String timeOfDay = (hours > 12) ? "PM" : "AM";
+        System.out.println(timeOfDay);
+
+
+
+        int income = 30000;
+
+        double taxRate = (income >= 40000) ? 0.25 : 0.15;
+        System.out.println(taxRate);
+
+    */
+    }
+}
